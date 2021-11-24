9e895b1b09ba8180442bc1e5246196f7882f9211
Gilberto Briscoe-Martinez <gilberto.briscoe-martinez@colorado.edu>
2021-11-24 09:38:52 -0700

    module name change

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='npm',
+    name='npm_base',
     author="Anuj Pasricha",
     author_email='anuj.pasricha@colorado.edu',
     version='1.0.0',
     description='Base project for all things NPM.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/HIRO-group/npm',
-    packages=setuptools.find_packages(where='npm*'),
+    url='https://github.com/HIRO-group/npm-base',
+    packages=setuptools.find_packages(where='npm_base*'),
     install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3.7',
