1bd71fa4954354068e86224439830baaaacb4fc6
Anuj Pasricha <anujpasricha01@gmail.com>
2022-11-21 22:49:47 -0700

    readability

--- npm_base/utils.py
+++ npm_base/utils.py
@@ -34,10 +34,10 @@ def write_txt(filepath, data):
 def convert_orientation(orientation, euler):
     if (type(orientation) == Point and euler) or (type(orientation) == Quaternion and not euler):
         return orientation
-    elif type(orientation) == Point:
-        return Quaternion(*pb.getQuaternionFromEuler(dc.astuple(orientation)))
-    elif type(orientation) == Quaternion:
-        return Point(*pb.getEulerFromQuaternion(dc.astuple(orientation)))
+    elif type(orientation) == Point and not euler:
+        return Quaternion(*pb.getQuaternionFromEuler(orientation.tolist()))
+    elif type(orientation) == Quaternion and euler:
+        return Point(*pb.getEulerFromQuaternion(orientation.tolist()))
 
 
 def pose_to_mat(pose):
